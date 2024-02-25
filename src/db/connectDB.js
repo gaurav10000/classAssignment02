73d0a695de12fe87d0fb1921df736352702af6f8
gaurav10000 <gaurav@gaurav-IdeaPad-5-15ALC05.sarasgaurav.com>
2024-02-25 11:55:24 +0530

    solved db-name problem

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -3,7 +3,9 @@ import { DB_NAME } from '../constants.js'
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`, {
+            dbName: DB_NAME
+        })
         console.log(`Successful connection to MongoDB: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error(error)
